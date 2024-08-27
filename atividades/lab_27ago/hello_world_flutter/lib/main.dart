63ec8d6af95275b4a31af5efea34d9187e2ea863
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-08-27 12:05:11 -0300

    lab 27 ago - interface mobile

--- atividades/lab_27ago/hello_world_flutter/lib/main.dart
+++ atividades/lab_27ago/hello_world_flutter/lib/main.dart
@@ -1,27 +1,64 @@
 /*
-  Ex 02 - Escrever "Hello World"
+  Ex 02 - LDDM
     1) sem usar widgets OK
     2) usando widgets OK
+
+  @owner: @catfmcastro
 */
 
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp()
-  );
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp (
-      title: 'Ex02',
-      home: Scaffold(
-        body: Center(
-          child: Text("Hello World")
-        )
-      )
-    );
+    return MaterialApp(
+        title: 'Ex02',
+        home: Scaffold(
+          appBar: AppBar(backgroundColor: Colors.blue,),
+          drawer: Drawer(),
+          bottomNavigationBar:
+              BottomNavigationBar(
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home), 
+                    label: "Home"
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: "Minha Conta")
+                ]
+              ),
+          body: Center(
+            child: Container(
+              width: 200,
+              child: Column (
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(labelText: "E-mail"),
+                    style: TextStyle(color: Colors.purple, fontSize: 20),
+                  ),
+                  const SizedBox(height: 20,),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Senha"),
+                    style: TextStyle(color: Colors.purple, fontSize: 20),
+                  ),
+                  
+                ],
+              )
+
+
+              // child: TextField(
+              //   decoration: InputDecoration(labelText: "Email: "),
+              //   style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+
+            )
+          )
+        );
   }
 }
-
