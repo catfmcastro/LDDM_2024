8b8959b83becc2c75ee27a0395cb2c6d464b962a
Catarina F. M. Castro <catfmcastro@gmail.com>
2024-09-17 11:28:13 -0300

    init pratica03

---
+++ atividades/pratica03/pratica03/lib/Login.dart
@@ -0,0 +1,78 @@
+/*
+  Pratica 01 - Interface
+
+  @owner: @catfmcastro - Catarina F. M. Castro
+  LDDM 2024_2
+*/
+
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Prática 01 - Interface',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: const Text("Login", style: TextStyle(color: Colors.white)),
+            iconTheme: IconThemeData(color: Colors.white),),
+          drawer: Drawer(),
+          bottomNavigationBar:
+              BottomNavigationBar(
+                items: <BottomNavigationBarItem>[
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.home), 
+                    label: "Home"
+                  ),
+                  BottomNavigationBarItem(
+                    icon: Icon(Icons.person),
+                    label: "Minha Conta")
+                ]
+              ),
+          body: Center(
+            child: Container(
+              width: 200,
+              child: Column (
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const TextField(
+                    decoration: InputDecoration(labelText: "E-mail"),
+                    style: TextStyle(color: Colors.purple, fontSize: 20),
+                  ),
+                  const SizedBox(height: 20,),
+                  const TextField(
+                    decoration: InputDecoration(labelText: "Senha"),
+                    style: TextStyle(color: Colors.purple, fontSize: 20),
+                  ),
+                  const SizedBox(height: 20,),
+                  Container (
+                    child: ElevatedButton(
+                      onPressed: () => {}, 
+                      child: Text("Entrar", style: TextStyle(color: Colors.white)),
+                      style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),)
+                  ),
+                  const SizedBox(height: 20,),
+                  Row (                    
+                    children: <Widget>[
+                      Expanded(
+                        child: Text("Novo aqui?"),
+                      ),
+                      Expanded (child: TextButton(onPressed: () => {}, child: Text("Criar conta", style: TextStyle(color: Colors.blue))),
+                        )
+                    ]
+                  )
+                ],
+              )
+
+
+              // child: TextField(
+              //   decoration: InputDecoration(labelText: "Email: "),
+              //   style: TextStyle(color: Colors.purple, fontSize: 20),
+              ),
+
+            )
+          )
+        );
+  }
+}
---
+++ atividades/pratica03/pratica03/lib/main.dart
@@ -0,0 +1,19 @@
+/*
+  Pratica 03 - Navegações
+
+  @owner: @catfmcastro - Catarina F. M. Castro
+  LDDM 2024_2
+*/
+
+import 'package:flutter/material.dart';
+import 'Login.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Login(),
+    )
+  );
+}
+
+
